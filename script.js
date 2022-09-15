22fdd22e4cfe15e8cdbbeb4bee948e5069109fa3
risaadodx <wrisaad@gmail.com>
2022-09-15 13:59:55 +0600

    seconds in round fixed

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken.toFixed(0)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -140,6 +140,6 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
   document.getElementById("show-time").innerHTML = `${
-    startTime ? timeSpent : 0
+    startTime ? timeSpent.toFixed(0) : 0
   } seconds`;
 }, 1000);
